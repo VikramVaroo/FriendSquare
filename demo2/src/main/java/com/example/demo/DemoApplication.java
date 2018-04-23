b5d0d00337f638c2c4297d70e3ff290c23e04fed
VikramVaroo <vikram.varoo@gmail.com>
2018-04-22 21:42:53 -0400

    first commit

---
+++ demo2/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		SpringApplication.run(DemoApplication.class,args);
+	}
+
+}
